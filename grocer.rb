3356fb67080a9c69c403e4148ff03d1951bda52b
Joe Jamsky <cba1067950@yahoo.com>
2019-05-16 13:09:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,26 +2,17 @@ require 'pry'
 
 def consolidate_cart(cart)
   # code here
-  cart.each_with_object({}) do |item, result|
+  newHash = {}
+  cart.each do |item|
     item.each do |type, attribute_hash|
-      if result[type]
+      if newHash[type]
         attribute_hash[:count] += 1
       else
         attribute_hash[:count] = 1
-        result[type] = attribute_hash
+        newHash[type] = attribute_hash
       end
     end
-    
   end
-
-  # cart.map do |item_hash|
-  #   key = item_hash.keys.join
-  #   #binding.pry
-  #   #num = cart.count(item_hash)
-  #   item_hash[key][:count] = cart.count(item_hash)
-  # # binding.pry
-  # end 
-  # cart.uniq
 end
 
 def apply_coupons(cart, coupons)
